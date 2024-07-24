2a70ec55635fb4e210696a622cb2d26966283a7f
tpaks <tpaks@DESKTOP-1NL0OLQ>
2024-07-24 22:39:00 +0900

    초기 커밋.

---
+++ WPF_Example_Opening Custom Window/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Example_Opening_Custom_Window.View;
+
+namespace WPF_Example_Opening_Custom_Window
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWindow normalWindow = new NormalWindow();
+            normalWindow.Show();
+        }
+
+        private void btnModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow(this);
+            Opacity = 0.4;
+            modalWindow.ShowDialog();
+            Opacity = 1;
+
+            if (modalWindow.Success)
+            {
+                txtinput.Text = modalWindow.Input;
+            }
+        }
+    }
+}+
